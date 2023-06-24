badb0c310a5997c9d7c4de5b0d33b841490dbcec
shartlord <115052495+shartlordyt@users.noreply.github.com>
2023-06-23 17:09:26 -0700

    Update main.js

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ function Dropdown() {
 
 
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbutton')) {
+  if (!event.target.matches('.dropbtn')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
