be92e4d5110338a005a0ba6f3e32d71386825d68
shartlord <115052495+shartlordyt@users.noreply.github.com>
2023-09-02 13:55:28 -0700

    Hey guys today we will be making a websitw

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    
+    document.getElementById("randomGameButton").addEventListener("click", getRandomGameURL);
+});
+
+
+
 function blank(){
       (function () {var url = document.URL; var urlObj = new window.URL(window.location.href); win = window.open(); win.document.body.style.margin = "0"; win.document.body.style.height = "100vh"; var iframe = win.document.createElement("iframe"); iframe.style.border = "none"; iframe.style.width = "100%"; iframe.style.height = "100%"; iframe.style.margin = "0"; iframe.referrerpolicy = "no-referrer"; iframe.allow = "fullscreen"; iframe.src = url.toString(); win.document.body.appendChild(iframe); var script = win.document.createElement("script"); script.src = ""; win.document.body.appendChild(script); })();
     }
@@ -116,7 +123,7 @@ function coolquote(){
         var randomGameURL = "apps/" + gameList[randomIndex];
 
         window.location.href = randomGameURL;
-	console.log("Button clicked!");
+	
     }
 
     document.getElementById("randomGameButton").addEventListener("click", getRandomGameURL);
